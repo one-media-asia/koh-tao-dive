9e4712a2ea165811a97fdf59f410263f5fcc7909
rocketasias-projects <onemediaasia@duck.com>
2026-03-17 22:57:28 +0800

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -174,7 +174,7 @@ const Navigation = ({ user, isAdmin, isAdminRoute }: { user?: any, isAdmin?: boo
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16 border-solid border-0">
           <div className="flex items-center">
-            <img src="/images/logo.png" alt="Pro Diving Asia Logo" className="h-20 w-auto" style={{ display: 'block', marginRight: 16 }} />
+            <img src="/images/logo.png" alt="Pro Diving Asia Logo" className="h-12 w-auto" style={{ display: 'block', marginRight: 8, marginLeft: 0 }} />
           </div>
           {/* Desktop nav */}
           <div className="hidden md:flex items-center space-x-8">
