69633ea3e29364e349179f605134fc1d1a5a41bf
rocketasia <onemediaasia@duck.com>
2026-03-24 04:59:39 +0800

    Create client.ts

---
+++ src/integrations/contentful/client.ts
@@ -0,0 +1,8 @@
+import { createClient } from 'contentful';
+
+const contentfulClient = createClient({
+  space: 'dxz3091tnbzu',
+  accessToken: 'YArIMzW5Pl74W0-ODfe0MAwFGvwoIeRtAacuh4m2iII',
+});
+
+export default contentfulClient;
