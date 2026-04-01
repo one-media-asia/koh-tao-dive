def3403753a9b9256c986bea276a05a2524d23a4
rocketasia <onemediaasia@duck.com>
2026-04-01 15:40:26 +0800

    s

--- admin/---pages/api/project-tasks.js
+++
@@ -1,6 +0,0 @@
-    if (error) return res.status(500).json({ error: error.message });
-    return res.status(204).end();
-  }
-  res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
-  res.status(405).end(`Method ${req.method} Not Allowed`);
-}
