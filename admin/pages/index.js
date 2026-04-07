186cc8e61c1d03b59b95d2275193c127d2c1cfcd
one-media-asia <onemediaasia@duck.com>
2026-04-07 11:33:08 +0800

    Create index.js

---
+++ admin/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Admin Dashboard</h1>
+      <p>Welcome! The admin panel is now working.</p>
+    </div>
+  );
+}
