5c044954c43d69b68a6acb549f794ab6b96d4cca
one-media-asia <onemediaasia@duck.com>
2026-04-09 17:39:21 +0800

    Update CoursePageTemplate.tsx

--- src/components/CoursePageTemplate.tsx
+++ src/components/CoursePageTemplate.tsx
@@ -147,7 +147,7 @@ const CoursePageTemplate: React.FC<CoursePageProps> = ({
 
   // Scroll to contact section
   const openBookNow = () => {
-    window.open('/booknow.html', '_blank');
+    window.open('/booking-form', '_blank');
   };
 
   return (
